93f240a0eae186e5e0b05921b812ad583e2995b0
bcollins <parietal.io@gmail.com>
2017-10-02 14:53:08 -0500

    updated download data script with new datasets

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,1 +1,6 @@
 wget http://parietal-outbox.s3.amazonaws.com/iucn_polygons_wgs84.geojson
+wget http://parietal-outbox.s3.amazonaws.com/world.shp
+wget http://parietal-outbox.s3.amazonaws.com/world.dbf
+wget http://parietal-outbox.s3.amazonaws.com/world.prj
+wget http://parietal-outbox.s3.amazonaws.com/world.shx
+wget http://parietal-outbox.s3.amazonaws.com/world.geojson
