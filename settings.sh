22ea8ef4be02629e0452f711973e14d18b14b497
brendancol <bcollins@parietal.io>
2017-10-02 12:06:44 -0500

    added settings file for IUCN stuff

---
+++ settings.sh
@@ -0,0 +1,3 @@
+export IUCN_ES_HOST = "https://search-icuntest-w752w6xxvrbcfrzb3lnmbgr24e.us-east-1.es.amazonaws.com"
+export IUCN_ES_INDEX = "myindex"
+export ICUN_ES_POLYGONS = "./data/iucn_polygons_wgs84.geojson"
