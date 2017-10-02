b87432dc61cdc3db35ddbd1c2563546e3a55ae2f
bcollins <parietal.io@gmail.com>
2017-10-02 11:46:12 -0500

    added notes in load data script

--- load-data.sh
+++ load-data.sh
@@ -1,1 +1,14 @@
-curl -XPUT 'http://localhost:9200/suburbs/'
+export IUCN_ES_HOST = "https://search-icuntest-w752w6xxvrbcfrzb3lnmbgr24e.us-east-1.es.amazonaws.com"
+export IUCN_ES_INDEX = "myindex"
+export ICUN_ES_POLYGONS = "./data/iucn_polygons_wgs84.geojson"
+export WEB_TUTORIAL = "https://mapbutcher.gitbooks.io/using-spatial-data-in-elasticsearch/content/exercise_2_-_indexing_some_spatial_data/create_index.html"
+
+echo $HOST
+echo $INDEX
+echo $POLYGONS
+
+# create empty index
+curl -XPUT '$HOST/$INDEX/'
+
+# add polygons
+ogr2ogr -progress --config ES_OVERWRITE ES_BULK=100 -f "ElasticSearch" "$HOST" "$POLYGONS" -nln $INDEX
