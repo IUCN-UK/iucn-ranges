053b878c7f5420bc9ae122219357935f9f6bed47
bcollins <parietal.io@gmail.com>
2017-10-02 14:01:33 -0500

    added first version of create index script

--- create-geohash-index.sh
+++ create-geohash-index.sh
@@ -4,13 +4,60 @@ curl -XPUT $IUCN_ES_HOST/$IUCN_ES_INDEX -d '
   "mappings": {
     "animals": {
       "properties": {
-        "name": {
+        "id_no": {
+          "type": "double"
+        },
+        "binomial": {
+          "type": "string"
+        },
+        "presence": {
+          "type": "integer"
+        },
+        "origin": {
+          "type": "integer"
+        },
+        "compiler": {
+          "type": "string"
+        },
+        "year": {
+          "type": "integer"
+        },
+        "citation": {
+          "type": "string"
+        },
+        "source": {
+          "type": "string"
+        },
+        "dist_comm": {
+          "type": "string"
+        },
+        "island": {
+          "type": "string"
+        },
+        "subspecies": {
+          "type": "string"
+        },
+        "subpop": {
+          "type": "string"
+        },
+        "data_sens": {
+          "type": "string"
+        },
+        "sens_comm": {
+          "type": "string"
+        },
+        "seasonal": {
+          "type": "integer"
+        },
+        "tax_comm": {
+          "type": "string"
+        },
+        "legend": {
           "type": "string"
         },
         "location": {
-          "type":               "geo_shape",
-          "geohash_prefix":     true, 
-          "geohash_precision":  "50km" 
+          "type": "geo_shape",
+          "precision": 7
         }
       }
     }
